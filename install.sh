0a55f71a4305934a04c7250a88e2a90b966bbfc1
Ryan Taylor Long <ryan@rtlong.com>
2015-11-06 15:34:38 -0800

    Make installer just give up if discovers existing dir at $DOTFILES_PATH

--- install.sh
+++ install.sh
@@ -6,14 +6,11 @@ if [[ ${DOTFILES_PATH:-unset} == 'unset' ]]; then
 	export DOTFILES_PATH="$HOME/.dotfiles"
 fi
 
-if [[ ! -d ${DOTFILES_PATH}/.git ]]; then
-	if [[ -d ${DOTFILES_PATH} ]]; then
-		echo "$DOTFILES_PATH already exists but isn't a git repo. Unsure how to continue."
-		exit 1
-	fi
+if [[ -d ${DOTFILES_PATH} ]]; then
+	echo "$DOTFILES_PATH already exists. Aborting."
+fi 
 
-	git clone https://github.com/GoodGuide/dotfiles.git "${DOTFILES_PATH}"
-fi
+git clone https://github.com/GoodGuide/dotfiles.git "${DOTFILES_PATH}"
 
 cd "${DOTFILES_PATH}"
 
