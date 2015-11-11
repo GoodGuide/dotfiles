5d1ee5c24df91133c0a7ae37cc9addb221c83e47
Ryan Taylor Long <ryan@rtlong.com>
2015-11-11 14:56:41 -0800

    Add test script
    [no story]

---
+++ test_shell.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+# vim: set noexpandtab tabstop=4 shiftwidth=0:
+
+set -x -e
+export DOTFILES_PATH="$PWD"
+export HOME="$(mktemp -d)"
+
+_DOTFILES_TEST=true $DOTFILES_PATH/install.sh
+
+cd $HOME
+set +x
+echo '++ Starting ZSH in new environment. Message will print when that shell exits'
+zsh -i -l
+echo "++ END OF $@ ++"
